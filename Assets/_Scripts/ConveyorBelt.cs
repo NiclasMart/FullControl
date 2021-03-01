107d001e23888f0cb708bd9a13c35d535f36d665
Niclas Mart <salcintram07@web.de>
2021-03-01 09:39:31 +0100

    implemented basic conveyor belt script

---
+++ Assets/_Scripts/ConveyorBelt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    [SerializeField] float maxSpeed = 1f;
+    [SerializeField] bool invertDirection = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        
+
+        if (rb != null) {
+            Vector2 direction = new Vector2(100f, 0);
+            Debug.Log("Beschleunigen");
+            rb.AddForce(direction);
+        }
+    }
+}
